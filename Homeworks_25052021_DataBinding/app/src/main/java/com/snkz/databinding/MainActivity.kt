a2d99ef8fa382c0b12c39341fc742209f236521e
SNKz0610 <blackaxe2202@gmail.com>
2021-05-31 23:27:58 +0700

    31052021

--- Homeworks_25052021_DataBinding/app/src/main/java/com/snkz/databinding/MainActivity.kt
+++ Homeworks_25052021_DataBinding/app/src/main/java/com/snkz/databinding/MainActivity.kt
@@ -2,23 +2,39 @@ package com.snkz.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.snkz.databinding.databinding.ActivityMainBinding
 
-data class Profile(val name : String, val company : String, val address : String, val numberPics : Int, val numberFollowrer : Int, val numberFollows : Int)
 
 class MainActivity : AppCompatActivity() {
-    private val snkz = Profile("Đô Anh", "Astralis", "Hà Nội", 10, 0, 1)
-    private lateinit var binding : ActivityMainBinding
+    private  var tag : String = "WelcomeActivity"
+    private lateinit var mainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.tvName.text = "${snkz.name}"
-        binding.tvSchool.text = "${snkz.company}"
-        binding.tvCountry.text = "${snkz.address}"
-        binding.tvNumberofphotos.text = "${snkz.numberPics}"
-        binding.tvNumberoffollowers.text = "${snkz.numberFollowrer}"
-        binding.tvNumberoffollows.text = "${snkz.numberFollows}"
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        var snkz = Profile("Đô Anh", "Astralis", "Hà Nội", 10, 0, 1)
+        mainBinding.binding = snkz
+        mainBinding.btnShownumber.setOnClickListener {
+            if (mainBinding.groupInformation.visibility == View.GONE) {
+                mainBinding.groupInformation.visibility = View.VISIBLE
+                mainBinding.btnShownumber.text = "Hide information"
+
+            } else {
+                mainBinding.groupInformation.visibility = View.GONE
+                mainBinding.btnShownumber.text = "Show information"
+            }
+
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(tag, "onResume")
+        Toast.makeText(this, "❤ Welcome Back ❤", Toast.LENGTH_LONG ).show()
     }
 }
